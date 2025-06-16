f362c878d6bd25d977fc66c4549129bca8a107e9
machufay <mattyangfei@gmail.com>
2025-06-16 11:19:37 -0500

    hui

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("four");
+let secretPassword = ("five");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -13,15 +13,29 @@ button.addEventListener("click", function() {
 
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
-    
+    if (username === "four" && password === "five") {
+        message.innerHTML = "success";
+    }
+
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
     //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
     //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
-    
-    
+    if (username ==="four" && password != "five"){
+        message.innerHTML = "wrong username";
+    } 
+    if (username != "four" && password === "five"){
+        message.innerHTML = "wrong password";
+    } 
+    if (username === "" && password === ""){
+        message.innerHTML = "enter something";
+    } 
+    if (username != "four" && password != "five"){
+        message.innerHTML = "incorrect username and password";
+    }
+
     // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
     //  - Hint: What string represents an "empty" message.
-    
+
 });
